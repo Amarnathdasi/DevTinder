08c925d91fcb0e2ac721046872954538323e0401
D Amarnath <dasiamarnath143d@gmail.com>
2025-08-13 19:49:09 +0530

    using next function

--- src/app.js
+++ src/app.js
@@ -2,19 +2,28 @@ const express=require("express")
 
 const app=express()
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Amarnath",secondName:"Dasi"})
+// app.get("/user",(req,res)=>{
+//     res.send({firstName:"Amarnath",secondName:"Dasi"})
+// })
+
+
+// app.use("/test",(req,res)=>{
+//      res.send("Hello Hello Hello")
+// })
+
+app.use("/user",(req,res,next)=>{
+    console.log("Route 1")
+    next()
+    res.send("Route 1")
+    
+},(req,res)=>{
+   res.send("Hi User")
+ console.log("Route 2")
+
 })
 
 
-app.use("/test",(req,res)=>{
-     res.send("Hello Hello Hello")
-})
 
-
-
-
-
-app.listen(3000,()=>{
+app.listen(7777,()=>{
     console.log("Sucessfully Server Listening on port 3000...")
 })
