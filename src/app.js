e0024dcd165c5fc6b42576d4b9bb46bbab326d4d
D Amarnath <dasiamarnath143d@gmail.com>
2025-08-13 21:12:14 +0530

    creating middleware

--- src/app.js
+++ src/app.js
@@ -11,17 +11,39 @@ const app=express()
 //      res.send("Hello Hello Hello")
 // })
 
-app.use("/user",(req,res,next)=>{
-    console.log("Route 1")
-    next()
-    res.send("Route 1")
+// app.use("/user",(req,res,next)=>{
+//     console.log("Route 1")
+//     next()
+//     // res.send("Route 1")
     
-},(req,res)=>{
-   res.send("Hi User")
- console.log("Route 2")
+// },(req,res,next)=>{
+// //    res.send("Hi User")
+//  console.log("Route 2")
+//  next()
 
+// })
+
+const {adminAuth,userAuth}=require("./middleware/auth.js")
+app.use("/admin",adminAuth)
+app.get("/admin/getAllData",(req,res)=>{
+    
+        res.send("send Data")
+   
 })
 
+app.get("/admin/deleteData",(req,res)=>{
+  
+    res.send("Data Deleted")
+  
+})
+
+app.get("/user/login",(req,res)=>{
+    res.send("user Login")
+})
+
+app.get("/user/getData",userAuth,(req,res)=>{
+    res.send("userData Send")
+})
 
 
 app.listen(7777,()=>{
